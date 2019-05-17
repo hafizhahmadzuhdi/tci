cad6119dffd9f5b1417ca0d17343ff2849477f48
Hafizh Ahmad Zuhdi <hafizhahmadzuhdi@gmail.com>
2019-05-17 11:13:52 +0200

    W3 red 2 DateExceptionTest

--- Week3/src/test/java/CourseTest.java
+++ Week3/src/test/java/CourseTest.java
@@ -1,9 +1,8 @@
 import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import Exception.*;
-
 
 import org.junit.runner.RunWith;
 
@@ -17,9 +16,21 @@ public class CourseTest {
  * All Tests are made based on the document requirements
  */
 
+    private static final Object[] getViolateCourses(){
+            return new Object[]{
+                    new Object[] {"ANDROID2", "03-05-2019", "02-05-2019"}
+            };
+    }
+
+    @Test
     public void constructorShouldGetNameOfCourse(){
         Course course = new Course("ANDROID1", "02-02-2019", "02-05-209");
         assertEquals("ANDROID1", course.getClassName());
     }
 
+    @Test(expected = CourseDateException.class)
+    @Parameters(method = "getCourses")
+    public void shouldThrowAnExceptionDate(String className, String beforeDate, String endDate){
+        Course course = new Course(className, beforeDate, endDate);
+    }
 }
